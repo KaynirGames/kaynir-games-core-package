54faaebe96ef3ec37b15820709dfcdb64e5d35f2
KaynirGames <kaynir.games@gmail.com>
2020-10-03 22:33:17 +0300

    Обновил версию Unity для пакета до 2020.1, добавил модульную систему управления персонажем (клавиатура, экранный джойстик).

---
+++ Runtime/InputSystem/Scripts/ScreenJoystickHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace KaynirGames.InputSystem
+{
+    public class ScreenJoystickHandler : InputHandler
+    {
+        [SerializeField] private Joystick _joystick = null;
+        [SerializeField] private ScreenJoybutton _attackButton = null;
+
+        private void Awake()
+        {
+            if (_snapMovementAxes)
+            {
+                _joystick.SnapX = true;
+                _joystick.SnapY = true;
+            }
+        }
+
+        public override Vector3 GetMovementInput()
+        {
+            Vector3 direction = Vector3.zero;
+
+            direction.x = _joystick.Horizontal;
+            direction.y = _joystick.Vertical;
+
+            return Vector3.ClampMagnitude(direction, 1f);
+        }
+
+        public override bool GetAttackInput()
+        {
+            return _attackButton.IsPressed;
+        }
+    }
+}
---
+++ Testing/Scripts/ControlsTest.cs
@@ -0,0 +1,27 @@
+﻿using KaynirGames.InputSystem;
+using KaynirGames.Movement;
+using UnityEngine;
+
+public class ControlsTest : MonoBehaviour
+{
+    [SerializeField] private CharacterMoveBase _characterMoveBase = null;
+    [SerializeField] private InputHandler _inputControl = null;
+
+    private Vector3 _moveDirection = Vector3.zero;
+
+    private void Update()
+    {
+        _moveDirection = _inputControl.GetMovementInput();
+        _characterMoveBase.SetMoveDirection(_moveDirection);
+
+        if (_inputControl.GetAttackInput())
+        {
+            Debug.Log("Attacking!");
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            Debug.Log("Attack button pressed.");
+        }
+    }
+}
